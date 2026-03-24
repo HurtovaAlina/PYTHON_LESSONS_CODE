3047f5a9e2c8d2a556f7775e7d90403206d9a6a8
HurtovaAlina <hurtovaalina2912@gmail.com>
2026-03-24 20:49:25 +0200

    new files and readme update

--- PythonProject_lesson_26_git/lesson_2.py
+++ PythonProject_lesson_26_git/lesson_2.py
@@ -7,9 +7,9 @@ import math
 print(math.sqrt(16))
 print(new(3, 4))
 
-def print_text(text):
+def print_text(text_1):
 
-    print(text)
+    print(text_1)
 
 print(print_text("Hello world"))
 
@@ -21,3 +21,8 @@ print(new_xy(3, 4))
 
 with open("new-text.txt", "r") as file:
     print(file.read())
+
+def add_xy(x, y):
+    return x + y
+
+print(add_xy(3, 4))
