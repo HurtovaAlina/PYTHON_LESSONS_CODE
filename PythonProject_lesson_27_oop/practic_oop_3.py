0c3fe33170d5d0811e57f83042923157beb926bb
HurtovaAlina <hurtovaalina2912@gmail.com>
2026-03-31 21:22:53 +0300

    lesson 2 oop new task

--- PythonProject_lesson_27_oop/practic_oop_3.py
+++ PythonProject_lesson_27_oop/practic_oop_3.py
@@ -17,79 +17,79 @@
 #  поповнення кошторису
 from typing import List, Dict
 
-# class Project:
-#
-#     def __init__(
-#             self,
-#             name: str,
-#             budget: float,
-#
-#     ):
-#         self.name = name
-#         self.budget = budget
-#
-#         self.expenses: float = 0
-#         self.is_finished: bool = False
-#         self.duration: int = 0
-#         self.tasks: List[str] = []
-#
-#     def show_info(self):
-#         print(f"Назва: {self.name}")
-#         print(f"Час виконання: {self.duration} днів")
-#         print("Необхідні задачі:")
-#
-#         if self.tasks:
-#             for i in range(0,len(self.tasks)):
-#                 print(f"{i}. {self.tasks[i]}")
-#         else:
-#             print("Немає задач")
-#
-#     def add_task(self, task_name):
-#         self.tasks.append(task_name)
-#
-#     def divide_tasks(self, task: str, subtasks:List[str]):
-#         self.tasks.remove(task)
-#         self.tasks.extend(subtasks)
-#
-#     def complete_task(self, task: str, expenses: float, time:int):
-#         if expenses > self.budget:
-#             print("Not enough budget!")
-#             return
-#
-#         self.tasks.remove(task)
-#         self.expenses += expenses
-#         self.duration += time
-#         self.budget -= expenses
-#         print(f"Task {task} was completed with duration {self.duration}")
-#         print(f"Your expenses {self.expenses}")
-#         print(f"Remaining budget {self.budget}")
-#
-#         if not self.tasks:
-#             self.is_finished = True
-#
-#     def add_budget(self, additional_budget: float):
-#         self.budget +=additional_budget
-#         print(f"Budget was increased, new budget: {self.budget}")
-#
-#
-#
-# project_1 = Project("new development", 30000)
-# project_1.show_info()
-# project_1.add_task("investigation")
-# project_1.add_task("prototype")
-# project_1.add_task("implementation")
-# project_1.add_task("testing")
-# project_1.add_task("user acceptance testing")
-#
-# project_1.show_info()
-# subtasks = ["idea", "variants of realisation", "conception", "resume with customer"]
-# project_1.divide_tasks("investigation", subtasks)
-#
-# project_1.show_info()
-#
-# project_1.complete_task("idea", 200, 2)
-#
-# project_1.add_budget(450)
+class Project:
+
+    def __init__(
+            self,
+            name: str,
+            budget: float,
+
+    ):
+        self.name = name
+        self.budget = budget
+
+        self.expenses: float = 0
+        self.is_finished: bool = False
+        self.duration: int = 0
+        self.tasks: List[str] = []
+
+    def show_info(self):
+        print(f"Назва: {self.name}")
+        print(f"Час виконання: {self.duration} днів")
+        print("Необхідні задачі:")
+
+        if self.tasks:
+            for i in range(0,len(self.tasks)):
+                print(f"{i}. {self.tasks[i]}")
+        else:
+            print("Немає задач")
+
+    def add_task(self, task_name):
+        self.tasks.append(task_name)
+
+    def divide_tasks(self, task: str, subtasks:List[str]):
+        self.tasks.remove(task)
+        self.tasks.extend(subtasks)
+
+    def complete_task(self, task: str, expenses: float, time:int):
+        if expenses > self.budget:
+            print("Not enough budget!")
+            return
+
+        self.tasks.remove(task)
+        self.expenses += expenses
+        self.duration += time
+        self.budget -= expenses
+        print(f"Task {task} was completed with duration {self.duration}")
+        print(f"Your expenses {self.expenses}")
+        print(f"Remaining budget {self.budget}")
+
+        if not self.tasks:
+            self.is_finished = True
+
+    def add_budget(self, additional_budget: float):
+        self.budget +=additional_budget
+        print(f"Budget was increased, new budget: {self.budget}")
+
+
+
+project_1 = Project("new development", 30000)
+project_1.show_info()
+project_1.add_task("investigation")
+project_1.add_task("prototype")
+project_1.add_task("implementation")
+project_1.add_task("testing")
+project_1.add_task("user acceptance testing")
+
+project_1.show_info()
+subtasks = ["idea", "variants of realisation", "conception", "resume with customer"]
+project_1.divide_tasks("investigation", subtasks)
+
+project_1.show_info()
+
+project_1.complete_task("idea", 200, 2)
+
+project_1.add_budget(450)
 
 # Завдання 2
 # Створіть клас Телефон з атрибутами:
