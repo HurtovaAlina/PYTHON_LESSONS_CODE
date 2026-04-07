7cd1049f6482be02eb9648bab51c1a393345c47e
HurtovaAlina <hurtovaalina2912@gmail.com>
2026-04-07 21:00:51 +0300

    practic

--- pythonProject_lesson_28_magic_methods/practic.py
+++ pythonProject_lesson_28_magic_methods/practic.py
@@ -77,72 +77,118 @@ from typing import List
 # Пройдіться циклом for по плейлисту та виведіть кожну
 # пісню на екран
 
-class Song:
+# class Song:
+#
+#     def __init__(self, name: str, author: str):
+#         self._name = name
+#         self._author = author
+#
+#     def __eq__(self, other):
+#         if isinstance(other, Song):
+#             return self._name == other._name and self._author == other._author
+#
+#         else:
+#             return False
+#
+#
+#     def __str__(self):
+#         return f"Song: name {self._name}, author {self._author}"
+#
+# song_1 = Song("Shape of You", "Ed Sheeran")
+# song_2 = Song("Blinding Lights", "The Weeknd")
+# song_3 = Song("Imagine", "John Lennon")
+#
+# print(song_1)
+# print(song_1 == song_2)
+#
+# class Playlist:
+#
+#     def __init__(self, songs:List[Song] ):
+#         self.songs = songs
+#
+#     def __len__(self):
+#         return len(self.songs)
+#
+#     def __contains__(self, item):
+#         return item in self.songs
+#
+#     def __iter__(self):
+#         return iter(self.songs)
+#
+#     def add_song(self, song):
+#         if song not in self.songs:
+#             self.songs.append(song)
+#         else:
+#             "Song is already in the list"
+#
+#     def remove_song(self, song):
+#         if song in self.songs:
+#             self.songs.pop(song)
+#         else:
+#             print(f"Song \"{song}\" is not in the list")
+#
+# song_4 = Song("Rolling in the Deep", "Adele")
+#
+# playlist = Playlist([song_1, song_2, song_3])
+#
+# print("Length of playlist")
+# print(len(playlist))
+#
+# for song in playlist.songs:
+#     print(song)
+#
+# playlist.add_song(song_4)
+#
+# for song in playlist.songs:
+#     print(song)
+#
+#
+# playlist.remove_song("Bad Guy")
+#
+# print (song_2 in playlist)
 
-    def __init__(self, name: str, author: str):
-        self._name = name
-        self._author = author
+# Завдання 3
+# Створіть клас Cart з атрибутами
+#  items – список товарів
+#  total – загальна ціна товарів
+# методи:
+#  __str__(self) – повертає рядок зі списком товарів
+#  __len__(self) – повертає кількість товарів
+#  __add__(self, other) – об’єднує 2 кошики та повертає
+# новий кошик
+# Створіть два кошики. Виведіть кількість товарів в кожному
+# з них. Виведіть самі кошики. Об’єднайте їх та виведіть
+# кількість товарів в новому кошику та товари в ньому
 
-    def __eq__(self, other):
-        if isinstance(other, Song):
-            return self._name == other._name and self._author == other._author
+class Cart:
 
-        else:
-            return False
-
+    def __init__(self, items: List[str], total: float):
+        self._items = items
+        self._total = total
 
     def __str__(self):
-        return f"Song: name {self._name}, author {self._author}"
-
-song_1 = Song("Shape of You", "Ed Sheeran")
-song_2 = Song("Blinding Lights", "The Weeknd")
-song_3 = Song("Imagine", "John Lennon")
-
-print(song_1)
-print(song_1 == song_2)
-
-class Playlist:
-
-    def __init__(self, songs:List[Song] ):
-        self.songs = songs
+        return f"Items in Cart {self._items}"
 
     def __len__(self):
-        return len(self.songs)
+        return len(self._items)
 
-    def __contains__(self, item):
-        return item in self.songs
+    def __add__(self, other):
+        if isinstance(other, Cart):
+            new_items = self._items + other._items
+            new_total = self._total + other._total
+        return Cart(new_items, round(new_total,2))
 
-    def __iter__(self):
-        return iter(self.songs)
+item_1 = "banana"
+item_2 = "apple"
+item_3 = "orange"
 
-    def add_song(self, song):
-        if song not in self.songs:
-            self.songs.append(song)
-        else:
-            "Song is already in the list"
+items_in_cart = Cart([item_1, item_2, item_3], 456.90)
 
-    def remove_song(self, song):
-        if song in self.songs:
-            self.songs.pop(song)
-        else:
-            print(f"Song \"{song}\" is not in the list")
+print(items_in_cart)
+print(len(items_in_cart))
 
-song_4 = Song("Rolling in the Deep", "Adele")
-
-playlist = Playlist([song_1, song_2, song_3])
-
-print("Length of playlist")
-print(len(playlist))
-
-for song in playlist.songs:
-    print(song)
-
-playlist.add_song(song_4)
-
-for song in playlist.songs:
-    print(song)
-
-
-playlist.remove_song("Bad Guy")
-
-print (song_2 in playlist)
+cart_2 = Cart(["cake", "chocolate"], 220.7)
+print(cart_2)
+merged_items = items_in_cart + cart_2
+print(merged_items)
+print(len(merged_items))
