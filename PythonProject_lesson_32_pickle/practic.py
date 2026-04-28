34f0e1fa7f103ca3c33a3b13695bdb19ae9c57c6
HurtovaAlina <hurtovaalina2912@gmail.com>
2026-04-28 21:15:23 +0300

    practic + pickle json files 3

--- PythonProject_lesson_32_pickle/practic.py
+++ PythonProject_lesson_32_pickle/practic.py
@@ -13,64 +13,64 @@ import json
 from typing import List, Dict
 
 
-# def add_item(items: List[str]) -> None:
-#     item = input("Enter item ")
-#     items.append(item)
-#     print(f"Item {item} was added")
-#
-#
-# def items_output(items: List[str]):
-#     print("Items list:")
-#     for item in items:
-#         print(item)
-#
-# def save_to_json(items:List[str]) -> None:
-#     with open("items.json", "w", encoding="utf-8") as file:
-#         json.dump(items, file)
-#         print("Saved to items.json")
-#
-#
-# def read_from_json() -> List[str] | None:
-#     try:
-#         with open("items.json", "r", encoding="utf-8") as file:
-#             data = json.load(file)
-#             return data if data is not None else []
-#     except FileNotFoundError:
-#         return []
-#
-#
-#
-# def save_to_picle(items:List[str]) -> None:
-#     with open("items.pickle", "wb") as file:
-#         pickle.dump(items, file)
-#         print("Saved to items.pickle")
-#
-#
-# def read_from_pickle() -> List[str] | None:
-#     try:
-#         with open("items.pickle", "rb") as file:
-#             data = pickle.load(file)
-#             return data if data is not None else []
-#     except FileNotFoundError:
-#         return []
-#
-#
-#
-# if __name__ == "__main__":
-#
-#     items = []
-#     for i in range(1,4):
-#         add_item(items)
-#
-#     items_output(items)
-#
-#     save_to_json(items)
-#     items_in_cart = read_from_json()
-#     print(f"Items from json {items_in_cart}")
-#
-#     save_to_picle(items)
-#     items_in_order = read_from_pickle()
-#     print(f"Items from pickle {items_in_cart}")
+def add_item(items: List[str]) -> None:
+    item = input("Enter item ")
+    items.append(item)
+    print(f"Item {item} was added")
+
+
+def items_output(items: List[str]):
+    print("Items list:")
+    for item in items:
+        print(item)
+
+def save_to_json(items:List[str]) -> None:
+    with open("items.json", "w", encoding="utf-8") as file:
+        json.dump(items, file)
+        print("Saved to items.json")
+
+
+def read_from_json() -> List[str] | None:
+    try:
+        with open("items.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return data if data is not None else []
+    except FileNotFoundError:
+        return []
+
+
+
+def save_to_picle(items:List[str]) -> None:
+    with open("items.pickle", "wb") as file:
+        pickle.dump(items, file)
+        print("Saved to items.pickle")
+
+
+def read_from_pickle() -> List[str] | None:
+    try:
+        with open("items.pickle", "rb") as file:
+            data = pickle.load(file)
+            return data if data is not None else []
+    except FileNotFoundError:
+        return []
+
+
+
+if __name__ == "__main__":
+
+    items = []
+    for i in range(1,4):
+        add_item(items)
+
+    items_output(items)
+
+    save_to_json(items)
+    items_in_cart = read_from_json()
+    print(f"Items from json {items_in_cart}")
+
+    save_to_picle(items)
+    items_in_order = read_from_pickle()
+    print(f"Items from pickle {items_in_cart}")
 
 # Завдання 2
 # Напишіть клас Student
@@ -226,14 +226,55 @@ loaded_from_pickle = load_pickle()
 print(f"Loaded from pickle: {loaded_from_pickle}")
 
 
-# Завдання 1
-# Напишіть програму для збереження даних про музичні
-# групи у вигляді словника, де ключ – назва групи, значення –
-# список альбомів.
-# Напишіть функціонал:
-#  додати новий гурт
-#  додати новий альбом
-#  зберегти дані через json
-#  зберегти дані через pickle
-#  завантажити дані через json
-#  завантажити дані через pickle
+# Контакти (телефонна книга)
+# Реалізуйте телефонну книгу.
+# Контакт містить:
+# ім’я
+# телефон
+# email
+# Функціонал:
+# додати контакт
+# видалити контакт
+# знайти контакт за ім’ям
+# показати всі контакти
+# зберегти/завантажити через json
+# зберегти/завантажити через pickle
+#
+# Alina: {
+#     "phone" : "+380667890909",
+#     "email" : "alina@gmail.com"
+# }
+#
+# Alisa:  {
+#     "phone" : "+38066900000",
+#     "email" : "alisa@gmail.com"
+# }
+
+
+def add_contact(phone_book: Dict[str, Dict[str, str]]):
+    contact_name = input("Enter contact name ")
+    contact_phone = input("Enter contact phone ")
+    contact_mail = input("Enter contact mail ")
+
+    if contact_name not in phone_book:
+        phone_book[contact_name] = {
+            "phone": contact_phone,
+            "email": contact_mail
+        }
+
+def save_to_json(phone_book: Dict[str, Dict[str, str]], filename: str = "phone_book.json"):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(phone_book, file, indent=4, ensure_ascii=False)
+
+def load_json(filename:str = "phone_book.json") -> Dict[str, Dict[str, str]]:
+    with open(filename, "r", encoding = "utf-8") as file:
+        return json.load(file)
+
+phone_book = {}
+
+add_contact(phone_book)
+print(phone_book)
+
+save_to_json(phone_book)
+loaded_phone_book = load_json()
+print(f"Loaded from json {loaded_phone_book}")
